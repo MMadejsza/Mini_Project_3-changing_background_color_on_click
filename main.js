779169e8ed88f36b336c14cde0de2ea7fce3c71e
Maciej <maciejmad16@gmail.com>
2021-01-03 13:45:26 +0000

    Created condition

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 
+document.body.addEventListener("click", changeBackground);
+function changeBackground(e) {
+    let x = e.clientX;
+    let y = e.clientY;
+    console.log(`${x}, ${y}`);
+
+    if (x % 2 == 0 && y % 2 == 0) {
+        document.body.style.backgroundColor = "green";
+    } else if (x % 2 !== 0 && y % 2 !== 0) {
+        document.body.style.backgroundColor = "red";
+    } else {
+        document.body.style.backgroundColor = "blue";
+    }
+}
